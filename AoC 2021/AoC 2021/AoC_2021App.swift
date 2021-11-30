dbb99dadd135f96aabcbddb1cb14b47a35f9e2f2
Bence Tóth <bence.toth.work@icloud.com>
2021-11-30 21:39:28 +0100

    init repo

---
+++ AoC 2021/AoC 2021/AoC_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AoC_2021App.swift
+//  AoC 2021
+//
+//  Created by Bence Tóth on 2021. 11. 30..
+//
+
+import SwiftUI
+
+@main
+struct AoC_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
